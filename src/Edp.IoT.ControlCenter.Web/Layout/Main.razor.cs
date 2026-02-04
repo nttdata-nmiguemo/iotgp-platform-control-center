1543766b1e2d9e8afc06dd8f0185b117f06b50b7
Nuno Moura <43907698+mouralx@users.noreply.github.com>
2026-02-04 19:06:38 +0000

    1st Commit

---
+++ src/Edp.IoT.ControlCenter.Web/Layout/Main.razor.cs
@@ -0,0 +1,5 @@
+namespace  Edp.IoT.ControlCenter.Web.Layout;
+public partial class Main
+{
+    bool sidebarExpanded = true;
+}
---
+++ src/Edp.IoT.ControlCenter.Web/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Edp.IoT.ControlCenter.Web;
+using Radzen;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.RootComponents.Add<App>("#app");
+
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddRadzenComponents();
+
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+});
+
+await builder.Build().RunAsync();
